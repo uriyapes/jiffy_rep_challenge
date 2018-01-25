c09dc247202b3102112e6e33905cf3761f564b14
uriyapes <uriyapes@gmail.com>
2018-01-25 17:20:26 +0200

    Added train_arabic_digits

---
+++ train_arabic_digits.py
@@ -0,0 +1,151 @@
+import math
+import tensorflow as tf
+import parse_libras
+import parse_arabic_digits
+import nearest_neighbor
+import numpy as np
+from tensorflow.python.ops import gen_nn_ops
+
+class Model(object):
+    def __init__(self):
+        pass
+
+    def get_dataset(self, dataset_name = "libras"):
+        if dataset_name == "libras":
+            self.dataset = parse_libras.Dataset()
+        else:
+            self.dataset = parse_arabic_digits.Dataset()
+
+    def build_model(self):
+        T, D = self.dataset.get_dimensions()
+        num_labels = self.dataset.get_num_of_labels()
+        num_channels = 1
+
+        self.batch_size = 300
+        self.patch_t_size = 5
+        self.patch_D_size = 1
+        depth = 16
+        num_hidden = 40
+        max_pool_percentage = 0.1
+        self.max_pool_window_size = round(max_pool_percentage * T)
+        max_pool_out_size = int(math.ceil(T / self.max_pool_window_size))
+        init_learning_rate = 2e-3
+
+        self.graph = tf.Graph()
+
+        with self.graph.as_default():
+            # Input data.
+            self.tf_train_dataset = tf.placeholder(
+                tf.float32, shape=(self.batch_size, T, D, num_channels), name="train_dataset_placeholder")
+            self.tf_train_labels = tf.placeholder(tf.float32, shape=(self.batch_size, num_labels),
+                                                  name="train_labels_placeholder")
+            # tf_valid_dataset = tf.constant(self.dataset.get_validation_set())
+            tf_test_dataset = tf.constant(self.dataset.get_test_set())
+
+            self.max_pool_window_size_ph = tf.placeholder(tf.int32, shape=())
+
+            # Variables.
+            layer1_weights = tf.Variable(tf.truncated_normal(
+                [self.patch_t_size, self.patch_D_size, num_channels, depth], stddev=0.1))
+            layer1_biases = tf.Variable(tf.zeros([depth]))
+
+            layer2_weights = tf.Variable(tf.truncated_normal(
+                [max_pool_out_size * D * depth, num_hidden], stddev=0.1))
+            layer2_biases = tf.Variable(tf.zeros([num_hidden]))
+
+            layer3_weights = tf.Variable(tf.truncated_normal(
+                [num_hidden, num_labels], stddev=0.1))
+            layer3_biases = tf.Variable(tf.zeros([num_labels]))
+
+            # Model.
+            def model(data):
+                conv = tf.nn.conv2d(data, layer1_weights, [1, 1, 1, 1], padding='SAME')
+                hidden = tf.nn.relu(conv + layer1_biases)
+                hidden = gen_nn_ops._max_pool_v2(
+                        hidden,
+                        ksize=[1, self.max_pool_window_size_ph, 1, 1],
+                        strides=[1, self.max_pool_window_size_ph, 1, 1],
+                        padding='SAME')
+                N = data.get_shape().as_list()[0]
+                reshape = tf.reshape(hidden, [N, 10 * D * depth])
+                hidden_no_relu = tf.matmul(reshape, layer2_weights) + layer2_biases
+                hidden = tf.nn.relu(hidden_no_relu)
+                return (tf.matmul(hidden, layer3_weights) + layer3_biases), hidden
+
+            # Training computation.
+            # Predictions for the training, validation, and test data.
+            logits, self.train_embed_vec = model(self.tf_train_dataset)
+            self.train_prediction = tf.nn.softmax(logits)
+
+            # valid_logits, _ = model(tf_valid_dataset)
+            # self.valid_prediction = tf.nn.softmax(valid_logits)
+
+            self.test_logits, self.test_embed_vec = model(tf_test_dataset)
+            self.test_prediction = tf.nn.softmax(self.test_logits)
+
+            self.loss = tf.reduce_mean(
+                tf.nn.softmax_cross_entropy_with_logits(labels=self.tf_train_labels, logits=logits))
+            # Optimizer.
+            # self.optimizer = tf.train.GradientDescentOptimizer(0.005).minimize(self.loss)
+            self.optimizer = self.build_optimizer(init_learning_rate)
+
+    def build_optimizer(self, init_learning_rate):
+        return tf.train.AdamOptimizer(init_learning_rate).minimize(self.loss)
+
+    def train_model(self):
+        num_steps = 200
+
+        train_dataset = self.dataset.get_train_set()
+        train_labels = self.dataset.get_train_labels()
+        # valid_labels = self.dataset.get_validation_labels()
+        test_labels = self.dataset.get_test_labels()
+
+        with tf.Session(graph=self.graph) as session:
+            tf.global_variables_initializer().run()
+            print('Initialized')
+            for step in range(num_steps):
+                offset = (step * self.batch_size) % (train_labels.shape[0] - self.batch_size)
+                batch_data = train_dataset[offset:(offset + self.batch_size), :, :, :]
+                batch_labels = train_labels[offset:(offset + self.batch_size), :]
+                feed_dict = {self.tf_train_dataset : batch_data, self.tf_train_labels : batch_labels,
+                        self.max_pool_window_size_ph : self.max_pool_window_size}
+                _, l, predictions, train_embed_vec = session.run(
+                    [self.optimizer, self.loss, self.train_prediction, self.train_embed_vec], feed_dict=feed_dict)
+                if (step % 50 == 0):
+                    print('batch_labels: {}'.format(np.argmax(batch_labels, 1)))
+                    print('predictions: {}'.format(np.argmax(predictions, 1)))
+                    print('Minibatch loss at step %d: %f' % (step, l))
+                    print('Minibatch accuracy: %.1f%%' % self.accuracy(predictions, batch_labels))
+                    # print('Validation accuracy: %.1f%%' % self.accuracy(
+                    #     self.valid_prediction.eval(), valid_labels))
+            print('Test accuracy: %.1f%%' % self.accuracy(self.test_prediction.eval(feed_dict={self.max_pool_window_size_ph: self.max_pool_window_size}), test_labels))
+            self.test_embed_vec_result = self.test_embed_vec.eval(feed_dict={self.max_pool_window_size_ph: self.max_pool_window_size})
+            print('Test accuracy for 1NN: %.3f' % self.run_baseline(train_embed_vec, train_labels,
+                                                            self.test_embed_vec_result , test_labels))
+
+
+        import collections
+        print collections.Counter(tuple(np.argmax(train_labels,1)+1))
+        print collections.Counter(tuple(np.argmax(test_labels,1)+1))
+
+
+    def accuracy(self, predictions, labels):
+        return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+                / predictions.shape[0])
+
+    def run_baseline(self, train_set, train_labels, test_set, test_labels):
+        nn = nearest_neighbor.NearestNeighbor()
+        return nn.compute_one_nearest_neighbor_accuracy(train_set, train_labels, test_set, test_labels)
+
+
+if __name__ == '__main__':
+    arabic_model = Model()
+    arabic_model.get_dataset("arabic")
+    # arabic_model.dataset.pca_scatter_plot(arabic_model.dataset.test_set)
+    print('1NN Baseline accuarcy: %.3f' % arabic_model.run_baseline(arabic_model.dataset.train_set,
+                                                                    arabic_model.dataset.train_labels,
+                                                                    arabic_model.dataset.test_set,
+                                                                    arabic_model.dataset.test_labels))
+    arabic_model.build_model()
+    arabic_model.train_model()
+    arabic_model.dataset.pca_scatter_plot(arabic_model.test_embed_vec_result)
